140293534d7e2f2fff97d94a729b07c632f6e769
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-08-11 12:40:50 +0300

    debug: do not skip release by default

--- packages/dart/cargokit/build_tool/lib/src/precompile_binaries.dart
+++ packages/dart/cargokit/build_tool/lib/src/precompile_binaries.dart
@@ -29,7 +29,7 @@ class PrecompileBinaries {
     this.androidNdkVersion,
     this.androidMinSdkVersion,
     this.tempDir,
-    this.skipRelease = true,
+    this.skipRelease = false,
   });
 
   final PrivateKey privateKey;
